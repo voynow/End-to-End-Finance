420c90cd201d9b8eaf620d336f8815e92c3470d4
jamie voynow <voynow99@gmail.com>
2021-08-09 22:10:28 -0400

    Initial implementation of data scraping functionality

--- test.py
+++ test.py
@@ -1,19 +1,34 @@
-import datetime
-
 import numpy as np
 import pandas as pd
-
 import matplotlib.pyplot as plt
-plt.style.use('ggplot')
 
 import yfinance as yf
 
-current_datetime = datetime.datetime.now()
+plt.style.use('ggplot')
 
-symbols = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]["Symbol"].values
 
+# get current snp symbols
+snp_wiki = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+symbols = snp_wiki[0]['Symbol'].values
+
+# access random subset of snp symbols
+size = 500
+random_idxs = np.random.choice(len(symbols), size=size, replace=False)
+symbols = symbols[random_idxs]
+
+# concatenate symbols into string for yfinance api
 space = " "
 symbols_string = space.join(symbols)
 symbols_string = symbols_string.replace(".", "-")
 
-data = yf.download(symbols_string, start='2000-01-01')
+# load data from 2010 onward
+start_date = '2010-01-01'
+data = yf.download(symbols_string, start=start_date)
+
+# Count number of nans in each row
+col_nan_counts = np.bincount(np.where(np.isnan(data.values))[1])
+
+# Find percentage of cols in DF with > 4% nan values (turns out this is only ~10-12% of all cols)
+nan_col_idx = np.where(col_nan_counts > len(data) // 25)[0]
+nan_cols = data.columns[nan_col_idx]
+data.drop(nan_cols, axis=1, inplace=True)
